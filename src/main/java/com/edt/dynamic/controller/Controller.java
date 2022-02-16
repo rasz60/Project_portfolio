b2fa4e4f91cb51841d4febca6598d270a6a02ebe
rasz60 <96821067+rasz60@users.noreply.github.com>
2022-02-16 13:32:16 +0900

    edt# go to next level.

---
+++ src/main/java/com/edt/dynamic/controller/Controller.java
@@ -0,0 +1,46 @@
+package com.edt.dynamic.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Controller
+ */
+@WebServlet("*.do")
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controller() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		actionDo(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+
+	protected void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		
+		
+	}
+}
---
+++ src/main/webapp/header.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<header id="header">
+        <div id="logo">
+            <a href="main.jsp">
+                <img src="resources/img/logo-basic.png">
+            </a>
+        </div>
+        <div id="search">
+            <input type="search" name="search" autofocus autocomplete="off" onfocus="this.placeholder=''" onblur="this.placeholder='brand, name, model . . .'">
+            <a href="#">
+                <img src="resources/img/search_btn2.png">
+            </a>
+        </div>
+        <div id="menu">
+            <ul>
+                <li>
+                    <a href="godraw.do">DRAW</a>
+                </li>
+                <li>
+                    <a href="goshop.do">SHOP</a>
+                </li>
+                <li>
+                    <a href="join.do">JOIN</a>
+                </li>
+                <li>
+                    <a href="login.do">LOGIN</a>
+                </li>
+            </ul>
+        </div>
+</header>
